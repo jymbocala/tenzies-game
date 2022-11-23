eb41cf5da38badc62b0b96518bdcea8375bad256
Jym Bocala <jym.bocala@gmail.com>
2022-11-23 23:39:11 +1100

    add nanoid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from "react";
 import "./App.css";
 import Die from "./components/Die";
+import { nanoid } from "nanoid";
 
 export default function App() {
+
+
   const [dice, setDice] = React.useState(allNewDice()); // sets the state of dice to the array of 10 random numbers
 
-  // a function to generate an array of 10 random numbers
+  // a function to generate an array of objects with 10 random numbers and isHeld set to false by default
   function allNewDice() {
-    let diceNums = [];
+    let newDice = [];
     for (let i = 0; i < 10; i++) {
-      diceNums.push(Math.ceil(Math.random() * 6));
+      newDice.push({
+        id: nanoid(),
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+      });
     }
-    return diceNums;
-  };
-
+    return newDice;
+  }
 
   // map over the dice state numbers array to generate 10 Die elements with the value of each number.
-  const diceElements = dice.map((die) => {
-    return <Die value={die} />;
-  });
+  const diceElements = dice.map((die) => (
+    <Die key={die.id} value={die.value} />
+  ));
 
   // a function to generate a new array of 10 random numbers and set the dice state to the new array
   function rollDice() {
@@ -28,9 +34,7 @@ export default function App() {
   return (
     <div className="App">
       <main>
-        <div className="dice-container">
-          {diceElements}
-        </div>
+        <div className="dice-container">{diceElements}</div>
         <button className="roll-btn" onClick={rollDice}>
           Roll
         </button>
